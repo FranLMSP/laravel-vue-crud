42d96ffaf84597941de97949c60d47618458b15e
Franco Colmenarez <francolmenagar2@hotmail.com>
2018-04-06 14:36:59 -0400

    Cleaning stuff for the application

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,19 +1,12 @@
-var urlUsers = 'https://jsonplaceholder.typicode.com/users';
 const vm = new Vue({
 	el: '#main',
 	created: function() {
-		this.getUsers();
+
 	},
 	data: {
-		lists: []
+
 	},
 	methods: {
-		getUsers: function() {
-			axios.get(urlUsers).then(response => {
-				this.lists = response.data
-			});
-		}
+
 	}
 });
-
-console.log(vm);
