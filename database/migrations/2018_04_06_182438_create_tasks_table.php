c2ca0773f55a819b723088c53f9d4655c55af541
Franco Colmenarez <francolmenagar2@hotmail.com>
2018-04-06 15:09:17 -0400

    Creating the resource controller and adapting the migration

--- database/migrations/2018_04_06_182438_create_tasks_table.php
+++ database/migrations/2018_04_06_182438_create_tasks_table.php
@@ -15,6 +15,9 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
+
+            $table->string('keep');
+
             $table->timestamps();
         });
     }
