a58f5df07d41fa03058b852fceb09c941b70d7dc
Franco Colmenarez <francolmenagar2@hotmail.com>
2018-04-09 17:37:29 -0400

    Generating seeds for tasks and showing the json of our resource methods

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Task;
 
 class TaskController extends Controller
 {
@@ -13,7 +14,9 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
+        $tasks = Task::get();
+
+        return $tasks;
     }
 
     /**
@@ -56,7 +59,9 @@ class TaskController extends Controller
      */
     public function edit($id)
     {
-        //
+        $task = Task::findOrFail($id);
+        //Form
+        return $task;
     }
 
     /**
@@ -79,6 +84,7 @@ class TaskController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $task = Task::findOrFail($id);
+        $task->delete();
     }
 }
